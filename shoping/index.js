6ddb78dedfc3aa646af7bba894468f4971d2bb99
ShohelR18 <85635745+ShohelR18@users.noreply.github.com>
2021-11-27 20:41:33 +0400

    Update index.js

--- shoping/index.js
+++ shoping/index.js
@@ -1,118 +1,87 @@
-  var removebtn = document.getElementsByClassName('btn-danger');
+var removeBtn = document.getElementsByClassName('btn-danger');
+var addElements = document.getElementsByTagName('tbody')[0];
 
-//   for(i = 0 ; i < removebtn.length ; i++){
-//     removebtn[i].addEventListener('click' , removebtns)
-// } 
-function removebtns(e){
-    var event = e.target 
-     event.parentElement.parentElement.remove()
-     grandTotal()
+function removeBtns(e){
+   var  removeBtn1 = e.target;
+    removeBtn1.parentElement.parentElement.remove();
+    grandTotal()
 }
 
-var  addTocart = document.getElementsByClassName(' btn-primary')
-for(i = 0 ; i <  addTocart.length ; i++){
-    addTocart[i].addEventListener('click' , addTOCart)
+var addTocart = document.getElementsByClassName('btn-primary');
+for( let i = 0 ; i < addTocart.length ; i++){
+ addTocart[i].addEventListener('click' , addTocarts )
+
 }
 
-
-function addTOCart(e){
-    var cartevents = e.target ;
-     var carteventsall = cartevents.parentElement
-    var parentAddCart = cartevents.parentElement.parentElement ;
-    // var itemimg = parentAddCart.children[0].src
-    // var itemname = parentAddCart.children[1].innerText
-    // var itemprice = parentAddCart.children[0].innerText
-    // var itempricetotals = parentAddCart.children[1].innerText
-    //   console.log (itemimg , itemname  , itemprice ,itempricetotals)
-
-    // there is  second way to make add to cart list down 
-
-     var itemimg = parentAddCart.children[0].src
-     var itemname = parentAddCart.children[1].innerText
-     var itemprice = carteventsall.children[0].innerText
-      var itempricetotals = carteventsall.children[1].innerText
-    //   console.log (itemimg , itemname  , itemprice)
+function addTocarts(e){
+ var addTocarts = e.target;
+ var parents = addTocarts.parentElement;
+ var imgSrc = parents.children[0].src;
+ var titleName = parents.children[1].innerText;
+ var price = parents.children[2].innerText; 
  
-    addTocartUpdate(itemimg , itemname  , itemprice , itempricetotals)
+addToCartupdate(imgSrc ,titleName ,price);
+
 }
 
-function addTocartUpdate(itemimg , itemname  , itemprice , itempricetotals){
-    var createContent = document.createElement('div');
-    createContent.classList.add('cart-row')
-    var itemscart = document.getElementsByClassName('cart-items')[0];
-    var cartitemTitel = document.getElementsByClassName('cart-item-title');
-     for( i = 0 ; i < cartitemTitel.length; i++){
-        // console.log(cartitemTitel[i])
-        if(cartitemTitel[i].innerText == itemname ){
-            alert('This Item Already Added To Your Cart')
-            return
-        }
-       
-     }
+function addToCartupdate(imgSrc ,titleName ,price){
 
-   
-      createContentsall = `
-      <img src="${itemimg}" class="cart-item-image" alt="2">
-  <span class="cart-item-title"> ${itemname}</span>
- 
-<span class="cart-price cart-colunm">${itemprice}</span>
- <span class="cart-price-total cart-colunm"> ${itempricetotals}</span>
-<div class="cart-quantity cart-colunm">
-  <input type="number" name="" value="1" class="cart-quantity-input">
-  <button class="btn btn-danger" type="button">Remove</button>
-</div> `
-createContent.innerHTML =  createContentsall ;
-    itemscart.append(createContent)
+var createEl = document.createElement('tr');
+var titleNames = document.getElementsByClassName('item-title');
+//  console.log(titleNames)
+ for( i = 0 ; i < titleNames.length; i++){
+    //  console.log(titleNames[i].innerText == titleName )
+   if(titleNames[i].innerText == titleName ){
+       alert('This Item Already Added To Your Cart');
+       return 
+   }
+}
+createEl.innerHTML = `<td><img src="${imgSrc}" class="item-img" alt=""></td>
+<td><h5 class="item-title">${titleName}</h5></td>
+<td> <h4 class="item-price">${price}</h4></td>
+<td><input type="number" class="item-qty" value='1'></td>
+<td><h4 class="item-sub-total"> AED 0</h4></td>
+<td><button class="btn btn-danger">Remove</button></td>`
 
-    for(i = 0 ; i < removebtn.length ; i++){
-        removebtn[i].addEventListener('click' , removebtns)
-    }
-    
-for(i = 0 ; i < inputs.length ; i++){
-    inputs[i].addEventListener('click' , updateElement)
+addElements.append( createEl);
+
+for(  i = 0 ; i < removeBtn.length ; i++){
+ removeBtn[i].addEventListener('click' , removeBtns)
+}
+for(  i = 0 ; i <inputs.length ; i++){
+inputs[i].addEventListener('click' , updateqty);
 }
 grandTotal()
 }
 
+var inputs = document.getElementsByClassName('item-qty');
 
-
-var inputs = document.getElementsByClassName('cart-quantity-input');
-    
-// for(i = 0 ; i < inputs.length ; i++){
-//     inputs[i].addEventListener('click' , updateElement)
-// } 
-
-function updateElement(e){
-    
-    var quantityupdate = e.target
-    var parentsup = quantityupdate.parentElement.parentElement ;
-    // console.log(parentsup)
-   var priceElement = parentsup.getElementsByClassName('cart-price')[0];
-   var priceElementTotal = parentsup.getElementsByClassName('cart-price-total')[0];
-   var priceElements = priceElement.innerText.replace('$' , ' ');
-    priceElementTotal.innerText = quantityupdate.value * priceElements ;
-
-
-//    console.log(priceElementTotal.innerText)
-   if(isNaN(quantityupdate.value) || quantityupdate.value <= 0){
-    quantityupdate.value = 1;
+function updateqty(event){
+   var parantEvent = event.target;
+   var parantEvents = parantEvent.parentElement.parentElement;
+ 
+   var prices = parantEvents.getElementsByClassName('item-price')[0];
+   
+   var sub_price = parantEvents.getElementsByClassName('item-sub-total')[0];
+   var cart_price = prices.innerText.replace('AED' , ' ')
+     sub_price.innerText = parantEvent.value * cart_price ;
+   
+   if(isNaN(parantEvent.value) || parantEvent.value <= 0){
+     parantEvent.value = 1;
    }
    grandTotal()
 }
-
 function grandTotal(){
-    var total = 0 ; 
-    var grands = document.getElementsByClassName('cart-grand-price')[0] ;
-    var cartTotals = document.getElementsByClassName('cart-price-total');
-    
-   for(i = 0 ; i < cartTotals .length ; i++){
-   var carttotalall =  parseInt(cartTotals [i].innerText.replace('$',' '))
-    //  console.log(typeof  carttotalall )
-      total += carttotalall 
 
-     }
-        grands.innerText = '$' + total ;
-   
-     }
+ var total = 0 ;
+var grands = document.getElementsByClassName('grand-total')[0];
+var sub_all = document.getElementsByClassName('item-sub-total');
+for(i = 0 ; i < sub_all.length ; i++){
+ var carttotalall =  parseInt(sub_all [i].innerText.replace('AED',' '));
+  
+    total += carttotalall ;
 
+   }
+   grands.innerText = 'AED '+ total;
 
+}
